396009696e7668aa000671fb9581fe9d079b0ed5
lisardo <lisardo.kist@gmail.com>
2019-04-17 14:22:41 +0200

    Add Applitools to project

--- tests/specs/browser.checkbox.spec.js
+++ tests/specs/browser.checkbox.spec.js
@@ -1,10 +1,24 @@
 import CheckboxPage from '../pageobjects/checkbox.page';
+import {Eyes, Target} from '@applitools/eyes.webdriverio';
 
 describe('checkboxes', () => {
     it('checkbox 2 should be enabled', () => {
         CheckboxPage.open();
         expect(CheckboxPage.firstCheckbox.isSelected()).toEqual(false);
         expect(CheckboxPage.lastCheckbox.isSelected()).toEqual(true);
+    });
+    const eyes = new Eyes();
+    eyes.setApiKey(process.env.APPLITOOLS_KEY);
+    it('prepare eyes', () => {
+        let currentSize = browser.execute(function () {
+            return {
+                width: Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
+                height: Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+            };
+        });
+        console.log(browser.capabilities.browserName);
+        eyes.open(browser, '', 'Image test', currentSize);
+        eyes.check('Applitools test', Target.window());
     });
 
     it('checkbox 1 should be enabled after clicking on it', () => {
